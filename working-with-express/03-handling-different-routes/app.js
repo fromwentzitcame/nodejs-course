8a5202d08aaac1940a9a03e906d04891669b0e83
Alex Wentz <wentz.alex@gmail.com>
2023-05-05 17:16:48 -0400

    update course work

--- working-with-express/03-handling-different-routes/app.js
+++ working-with-express/03-handling-different-routes/app.js
@@ -3,41 +3,16 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-// app.use('/', (req, res, next) => {
-//     console.log('This always runs!');
-//     next();
-// });
-
-// app.use('/add-product', (req, res, next) => {
-//   console.log('In another middleware!');
-//   res.send('<h1>The "Add Product" Page</h1>');
-// });
-
-// app.use('/', (req, res, next) => {
-//   console.log('In another middleware!');
-//   res.send('<h1>Hello from Express!</h1>');
-// });
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/add-product', (req, res, next) => {
-    console.log('In another middleware!');
-    res.send(
-        '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Submit</button></form>'
-    );
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-// app.post() only fires for incoming POST requests instead of any kind
-app.post('/product', (req, res, next) => {
-  // body by default doesn't try to parse, so we imported bodyParser
-  console.log(req.body);
-  // redirects on submit to endpoint passed in
-  res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-    console.log('In another middleware!');
-    res.send('<h1>Hello from Express!</h1>');
-});
+app.use((req,res,next) => {
+  res.status(404).send('<h1>Page Not Found</h1>');
+})
 
 app.listen(3000);
